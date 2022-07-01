b1e6e62dea575627a19cec8cb280ae31f3685d47
ZipshowXR <zipshowxr@gmail.com>
2022-07-01 17:14:04 +0900

    a

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebAR-WebApp04A.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebAR-WebApp04B.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
